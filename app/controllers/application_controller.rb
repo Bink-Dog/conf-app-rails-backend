4317f80e009833b98d2f2b1323edbb505577a772
Miss-Cheese <nat.galligan@gmail.com>
2020-05-11 20:55:29 -0400

    Streamlined auth methods and put them all in the auth controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::API
 
+    def encode_token(id)
+        JWT.encode({user_id: id}, ENV["secret_key"])
+    end
+
+
     def get_auth_header
         request.headers["Authorization"]
     end
 
-    def decoded_token(token)
+
+    def decoded_token
         begin
             JWT.decode(get_auth_header, ENV["secret_key"])[0]["user_id"]
         rescue
@@ -12,8 +18,13 @@ class ApplicationController < ActionController::API
         end
     end
 
+
     def session_user
         User.find_by(id: decoded_token)
+    end
+
+    def logged_in?
+        !!session_user
     end
 
 end
