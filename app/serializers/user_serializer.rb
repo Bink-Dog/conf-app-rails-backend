6f2d5d0dbe048162ed118fb2be9e8266028e0198
Miss-Cheese <nat.galligan@gmail.com>
2020-06-02 19:24:02 -0400

    Added events for users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :uid, :auth_provider, :auth_token, :twitter_handle, :github_username, :company, :bio, :image_url
+  attributes :id, :email, :name, :uid, :auth_provider, :auth_token, :twitter_handle, :github_username, :company, :bio, :image_url, :events
 end
