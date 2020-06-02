f5fe7f0dfc8b8bd25767ec16134f6e3a91d59bb3
Miss-Cheese <nat.galligan@gmail.com>
2020-06-02 16:34:35 -0400

    Added events table and join UserEvent table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     validates :email, presence: true
     validates :email, uniqueness: { case_sensitive: false }
 
-    # has_secure_password
+    has_many :user_events
+    has_many :events, through: :user_events
+
 end
