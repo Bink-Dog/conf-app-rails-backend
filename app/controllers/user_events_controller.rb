28653325dc2d35567816fa3682e8491421b21ace
Miss-Cheese <nat.galligan@gmail.com>
2020-06-18 21:37:37 -0400

    Signing up for events implemented

---
+++ app/controllers/user_events_controller.rb
@@ -0,0 +1,13 @@
+class UserEventsController < ApplicationController
+
+    def index
+        user_event = UserEvent.all 
+        render json: user_event
+    end
+
+    def create
+        user_event = UserEvent.create(user_id: params[:user_id], event_id: params[:event_id])
+        render json: user_event
+    end
+    
+end
