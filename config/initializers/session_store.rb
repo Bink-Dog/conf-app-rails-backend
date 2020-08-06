a5cb98e4779b5a4b37a6314857b7fb3325219425
Kevin Galligan <kgalligan@gmail.com>
2020-08-06 14:11:36 -0400

    same site

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_event_bit', domain: 'conf-game.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_event_bit', domain: 'conf-game.herokuapp.com', same_site: :none
 else
-  Rails.application.config.session_store :cookie_store, key: '_event_bit'
+  Rails.application.config.session_store :cookie_store, key: '_event_bit', same_site: :none
 end
