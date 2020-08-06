57777c7d331fe27779617d334d7156e813c658dc
Kevin Galligan <kgalligan@gmail.com>
2020-08-06 14:18:36 -0400

    secure store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_event_bit', domain: 'conf-game.herokuapp.com', same_site: :none
+  Rails.application.config.session_store :cookie_store, key: '_event_bit', domain: 'conf-game.herokuapp.com', same_site: :none, secret: "hazelnut", secure: true
 else
   Rails.application.config.session_store :cookie_store, key: '_event_bit', same_site: :none
 end
